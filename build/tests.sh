b19638746edfb40d478da602c89a1c8ca3ef7561
Hakan Saplakoglu <hakansaplakog@gmail.com>
2023-03-23 14:03:12 -0400

    fixed array name in tests.sh

--- build/tests.sh
+++ build/tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-test-locations=("/mounts/nese-hdd" "/mounts/nese-ssd")
+testLocations=("/mounts/nese-hdd" "/mounts/nese-ssd")
 
-for i in "${test-locations[@]}"
+for i in "${testLocations[@]}"
 do
     echo "Testing $i..."
     fio --filename=$i/file --size=100m --direct=1 --rw=randread --bs=4k --ioengine=libaio --iodepth=1 --runtime=60 --time_based --group_reporting --name=latency-test-job --write_lat_log=read4k
